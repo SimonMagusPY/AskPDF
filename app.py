cfc35ccbabca87ed2d0bfa1da0b881bb06758211
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 10:50:04 +0200

    add langchain splitter and chunking

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def main(): 
     load_dotenv()
@@ -16,7 +17,16 @@ def main():
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
-        st.write(text)
+        #split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        st.write(chunks)
 
 if __name__ == '__main__':
     main()
