c782f7376b71ca905242a72df4f4ec1253437ba4
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 10:00:22 +0200

    add pdf reader and test file upload in streamlit

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from PyPDF2 import PdfReader
 
 def main(): 
     load_dotenv()
     st.set_page_config(page_title="Ask Your PDF")
     st.header("Ask your PDF 💬")
-
-    pdf = st.file_uploader("Upload you PDF", type='pdf')
     
-    
-
-    
-
+    #upload the file
+    pdf = st.file_uploader("Upload you PDF", type='pdf')  
+    #extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        st.write(text)
 
 if __name__ == '__main__':
     main()
