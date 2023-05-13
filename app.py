6025075621e7d4522ec7eec06835f629c407bf0f
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 09:45:24 +0200

    initila config of streamlit

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 def main(): 
     load_dotenv()
-    print(os.getenv("OPENAI_API_KEY"))
+    st.set_page_config(page_title="Ask Your PDF")
+    st.header("Ask your PDF 💬")
+
+    pdf = st.file_uploader("Upload you PDF", type='pdf')
+    
     
 
     
