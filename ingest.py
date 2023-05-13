8d8419145bb4a1bb405bad8d561b578af1aed305
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 19:41:42 +0200

    change code to add comments

--- ingest.py
+++ ingest.py
@@ -1,14 +1,34 @@
+"""
+This function extracts chunks of text from a PDF file and creates a knowledge base of the text using OpenAI embeddings.
+
+The function takes a PDF file as input and returns a knowledge base.
+"""
+
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
+"""
+    Extracts chunks of text from a PDF file and creates a knowledge base of the text using OpenAI embeddings.
 
+    Args:
+        pdf: A PDF file.
+
+    Returns:
+        A knowledge base.
+"""
 def extract_chunks(pdf):
+    # Check if the PDF file is valid
+    if not pdf:
+        raise ValueError('Please provide a valid PDF file.')
+    
+    # Extract the text from the PDF file
     pdf_reader = PdfReader(pdf)
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
+    #Split text into chunks
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
@@ -16,7 +36,7 @@ def extract_chunks(pdf):
         length_function=len
     )
     chunks = text_splitter.split_text(text)
-    
+    #Create embeddings for the chunks
     embeddings = OpenAIEmbeddings()
     knowledge_base = FAISS.from_texts(chunks, embeddings)
     
--- qa.py
+++ qa.py
@@ -1,3 +1,9 @@
+"""
+This function gets an answer to a question from a knowledge base created from a PDF file using OpenAI embeddings.
+
+The function takes a knowledge base and a question as input and returns an answer.
+"""
+
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
