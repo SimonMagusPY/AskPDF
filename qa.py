bb76f31be2ac69adba1db002e54072fe1e8f26cd
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 19:42:49 +0200

    first est to see if chatgpt API works

--- qa.py
+++ qa.py
@@ -5,14 +5,15 @@ The function takes a knowledge base and a question as input and returns an answe
 """
 
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain.llms import ChatGPT
 from langchain.callbacks import get_openai_callback
 
 
 def get_answer(chunks, question):
     docs = chunks.similarity_search(question)
 
-    llm = OpenAI()
+    llm = ChatGPT()
+
     chain = load_qa_chain(llm, chain_type="stuff")
 
     with get_openai_callback() as cb:
