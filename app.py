9f764b8b0bf6a7e56fe338b5caa17343fc849262
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 11:25:07 +0200

    load langchain Q&A and use OpenAI to answer question

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 def main(): 
     load_dotenv()
@@ -34,7 +36,12 @@ def main():
         user_question = st.text_input("Ask a question about your PDF:")
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
-            st.write(docs)
+
+        llm = OpenAI()    
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents=docs, question=user_question)
+
+        st.write(response)          
 
 
 
