11a618121e40f43b11d8e1f59d153f4fb04cc953
Simon Magus <simonmagusgno@gmail.com>
2023-05-13 11:47:31 +0200

    Finalise V1 - working but using expensive model

--- app.py
+++ app.py
@@ -29,19 +29,29 @@ def main():
             length_function=len
         )
         chunks = text_splitter.split_text(text)
+
         #create embeddings
         embeddings = OpenAIEmbeddings()
         knowledge_base = FAISS.from_texts(chunks, embeddings)
+        
+        #initialize docs to None
+        docs = None 
         #show user input for question
         user_question = st.text_input("Ask a question about your PDF:")
+    
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
+        
+        if docs:
 
-        llm = OpenAI()    
-        chain = load_qa_chain(llm, chain_type="stuff")
-        response = chain.run(input_documents=docs, question=user_question)
+            llm = OpenAI()    
+            chain = load_qa_chain(llm, chain_type="stuff")      
 
-        st.write(response)          
+            response = chain.run(input_documents=docs, question=user_question)            
+
+            st.write(response)
+        else:
+            st.write("Please enter a question to get an answer.")        
 
 
 
