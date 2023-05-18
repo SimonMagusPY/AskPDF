19604c9aa8e26bd3d37a29c215990f98a1ce9e59
Simon Magus <simonmagusgno@gmail.com>
2023-05-18 13:54:43 +0200

    experimet with writing embed to screen

--- qa.py
+++ qa.py
@@ -7,10 +7,11 @@ The function takes a knowledge base and a question as input and returns an answe
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import streamlit as st
 
 def get_answer(chunks, question):
     docs = chunks.similarity_search(question)
-
+    
     llm = OpenAI(model_name="text-curie-001")
 
     chain = load_qa_chain(llm, chain_type="stuff")
